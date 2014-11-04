e5fbfb67f3372cc79fc771ccef8089567b4886b9
unknown <belevicha@WS136-A8.us.ri.com>
2014-11-04 11:56:00 -0800

    JS lists

--- app/assets/javascripts/reactlists.jsx
+++ app/assets/javascripts/reactlists.jsx
@@ -13,27 +13,37 @@ var ReactListsMixin = {
   leftItems: function() {
     return this.props.data.items.filter(this.isOnLeftSide);
   },
+
+  renderLeftItem: function(item) { return this.renderItem(item, "left"); },
+  renderRightItem: function(item) { return this.renderItem(item, "right"); },
   
-  renderItem: function(item) { return (
-    <ReactListsItem key={item.id} item={item} onDoubleClick={this.move} />
+  renderItem: function(item, list) { return (
+    <ReactListsItem key={"" + list + item.id} item={item} onDoubleClick={this.move} />
 //    <div key={item.id} onDblClick={ this.move(item.id)}>{ item.caption }</div>
   );},
   
   move: function(id) {
-    var index = this.props.data.items.findIndex(item => item.id === id);
+    var index = this.state.right.findIndex(item => item.id === id);
+
     if (index >= 0)
-      this.state.right.push(this.props.data.items[index]);
-      this.setState({ right: this.state.right });
+        this.state.right.splice(index, 1);
+    else {
+      index = this.props.data.items.findIndex(item => item.id === id);
+      if (index >= 0)
+        this.state.right.push(this.props.data.items[index]);
+    }
+
+    this.setState({ right: this.state.right });
   },
 
   render: function() { return (
     <div className={this.props.width + " reactlists"}>
       <p>{this.props.title}</p>
       <div className={this.props.widthLeft + " reactlist"}>
-        { this.leftItems().map(this.renderItem) }
+        { this.leftItems().map(this.renderLeftItem) }
       </div>
       <div className={this.props.widthRight + " reactlist"}>
-        { this.state.right.map(this.renderItem) }
+        { this.state.right.map(this.renderRightItem) }
       </div>
       <p></p>
     </div>
